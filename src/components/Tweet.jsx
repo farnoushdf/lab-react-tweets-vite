0a923d3c97a2f98bc598b37f141d5e978af8a6ca
Farnoush Daliran <farnoushfdf@gmail.com>
2024-05-29 10:12:07 +0200

    correct image

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -7,41 +7,16 @@ import User from "./User";
 function Tweet({tweet}) {
   return (
     <div className="tweet">
-      {/* <img
-        src={tweet.user.image}
-        className="profile"
-        alt="profile"
-      /> */}
        <ProfileImage image={tweet.user.image} />
-
       <div className="body">
         <div className="top">
-          <span className="user">
-            {/* <span className="name">{tweet.user.name}</span> */}
-            <User name={tweet.user.name} handle={tweet.user.handle} />
-            <span className="handle">{tweet.user.handle}</span>
-          </span>
-
-          {/* <span className="timestamp">{tweet.timestamp}</span> */}
+            <User name={tweet.user.name} handle={tweet.user.handle} />     
           <Timestamp time={tweet.timestamp} />
         </div>
-
-        {/* <p className="message">
-          {tweet.message}
-        </p> */}
         <Message message={tweet.message} />
-
-        {/* <div className="actions"> */}
-          {/* Font Awesome icons */}
-          {/* <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i> */}
-        {/* </div> */}
         <Actions />
       </div>
       
-
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
